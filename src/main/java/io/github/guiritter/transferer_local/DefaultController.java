afeba76c34140852b1e4dc31a856cee572511ea8
Guilherme Alan Ritter <gui.a.ritter@gmail.com>
2020-04-06 10:45:40 -0300

    trying https://stackoverflow.com/users/1731935/manuel 's updated answer

--- src/main/java/io/github/guiritter/transferer_local/DefaultController.java
+++ src/main/java/io/github/guiritter/transferer_local/DefaultController.java
@@ -1,17 +1,15 @@
 package io.github.guiritter.transferer_local;
 
-import static org.springframework.http.MediaType.APPLICATION_OCTET_STREAM_VALUE;
+import static org.springframework.http.MediaType.APPLICATION_OCTET_STREAM;
 
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 
-import javax.servlet.http.HttpServletResponse;
-
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.rest.webmvc.RepositoryRestController;
-// import org.springframework.http.ResponseEntity;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.method.annotation.StreamingResponseBody;
@@ -27,37 +25,21 @@ public class DefaultController {
 	private String filePath;
 
 	@GetMapping("download")
-	public StreamingResponseBody downloadHub(HttpServletResponse response) throws IOException {
+	public ResponseEntity<StreamingResponseBody> downloadHub() throws IOException {
 		File file = new File(filePath + fileName);
-		response.setContentType(APPLICATION_OCTET_STREAM_VALUE);
-		response.setHeader("Content-Disposition", "attachment; filename=" + fileName);
-		response.setHeader("Content-Length", file.length() + "");
 		InputStream inputStream = new FileInputStream(file);
-		return outputStream -> {
-			int nRead;
-			byte[] data = new byte[1024*1024];
-			while ((nRead = inputStream.read(data, 0, data.length)) != -1) {
-				outputStream.write(data, 0, nRead);
-			}
-			inputStream.close();
-		};
+		return ResponseEntity
+				.ok()
+				.contentType(APPLICATION_OCTET_STREAM)
+				.header("Content-Disposition", "attachment; filename=" + fileName)
+				.header("Content-Length", file.length() + "")
+				.<StreamingResponseBody>body(outputStream -> {
+					int nRead;
+					byte[] data = new byte[1024*1024];
+					while ((nRead = inputStream.read(data, 0, data.length)) != -1) {
+						outputStream.write(data, 0, nRead);
+					}
+					inputStream.close();
+				});
 	}
-
-	// @GetMapping("download")
-	// public ResponseEntity<StreamingResponseBody> downloadHub(HttpServletResponse response) throws IOException {
-	// 	File file = new File(filePath + fileName);
-	// 	response.setContentType(APPLICATION_OCTET_STREAM_VALUE);
-	// 	response.setHeader("Content-Disposition", "attachment; filename=" + fileName);
-	// 	response.setHeader("Content-Length", file.length() + "");
-	// 	InputStream inputStream = new FileInputStream(file);
-
-	// 	return ResponseEntity.ok(outputStream -> {
-	// 		int nRead;
-	// 		byte[] data = new byte[1024*1024];
-	// 		while ((nRead = inputStream.read(data, 0, data.length)) != -1) {
-	// 			outputStream.write(data, 0, nRead);
-	// 		}
-	// 		inputStream.close();
-	// 	});
-	// }
 }
